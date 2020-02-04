6d38543ed89512087278d33e301cda41b0affaa3
eric lane <ericlane87@gmail.com>
2020-02-03 22:03:29 -0500

    added random view

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,35 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
 
-        //[Route("Movies/released/{year}/{month:range:regex(\\d{2})}:range(1,12")]
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
-            //ViewData["Movie"] = movie;
-          //  ViewBag.Movie = movie;
-
-             return View(movie);
-
-            // return new ViewResult();
-            // return Content("Hello World!");
-            // return HttpNotFound();
-            // return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-            }
-
- 
-            public ActionResult Edit(int id)
+            var customers = new List<Customer>()
             {
-                return Content("id="+ id);
-            }
+                new Customer { Name = "customer 1" },
+                new Customer { Name = "customer 1" }
+            };
+
+            var ViewModel = new RandomViewModel
+            {
+
+                Movies = movie,
+                Customers = customers
+            };
+            return View(ViewModel);
+        
+
+
+
+
+        //[Route("Movies/released/{year}/{month:range:regex(\\d{2})}:range(1,12")]
+
+
+        //ViewData["Movie"] = movie;
+        //  ViewBag.Movie = movie;
+
+        //return View(movie);
+
+        // return new ViewResult();
+        // return Content("Hello World!");
+        // return HttpNotFound();
+        // return new EmptyResult();
+        //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+    
+
+}
+            //public ActionResult Edit(int id)
+            //{
+            //    return Content("id="+ id);
+            //}
         
         public ActionResult Index(int? pageIndex,string sortBy)
         {
@@ -49,6 +69,7 @@ namespace Vidly.Controllers
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+
 
         
              public ActionResult ByReleaseDate(int year,int month)
